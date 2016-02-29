0fe51a109e5fcbcb4c29863231600af3de7be8bf
Per Rovegård <per@rovegard.se>
2016-02-29 13:54:39 +0100

    Added a test for appending to require.paths

---
+++ tests/misc/paths/program.js
@@ -0,0 +1,12 @@
+var test = require("../../test");
+
+var currentDir = new java.io.File(__DIR__);
+var subDir = new java.io.File(currentDir, "sub");
+
+try {
+  require.paths.push(subDir.toString());
+  var a = require("a");
+  test.assert(a.alive() === "yes", "it should be possible to add new root paths");
+} finally {
+  require.paths.pop();
+}
