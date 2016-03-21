d8c5584399c19ad77de117965d3c45040b4b20f0
Per Rovegård <per@rovegard.se>
2016-03-21 14:20:51 +0100

    Fixed formatting in tests/test.js

--- tests/test.js
+++ tests/test.js
@@ -1,15 +1,13 @@
-
 exports.print = typeof print !== "undefined" ? print : function () {
-    var system = require("system");
-    var stdio = system.stdio;
-    stdio.print.apply(stdio, arguments);
+  var system = require("system");
+  var stdio = system.stdio;
+  stdio.print.apply(stdio, arguments);
 };
 
 exports.assert = function (guard, message) {
-    if (guard) {
-        exports.print('PASS ' + message, 'pass');
-    } else {
-        exports.print('FAIL ' + message, 'fail');
-    }
+  if (guard) {
+    exports.print('PASS ' + message, 'pass');
+  } else {
+    exports.print('FAIL ' + message, 'fail');
+  }
 };
-
